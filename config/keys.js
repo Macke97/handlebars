1c081ea51e3a16da5a62674f7ab61660cd214f79
Unknown <marcusthelin97@gmail.com>
2018-08-17 20:33:35 +0200

    missed

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  google: {
+    clientID: '243566678176-f53qppv0p38umasbmndbnnpldeva52fs.apps.googleusercontent.com',
+    clientSecret: 'eugUYGuIpUmOroHd0iWp23sW'
+  },
+  session: {
+    keys: ['verysecretstringyouknow']
+  }
+}
